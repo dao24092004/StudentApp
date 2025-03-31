d07847de0e3c85ce25273d1809b1ae1b57dc604d
dao24092004 <134945882+dao24092004@users.noreply.github.com>
2025-04-01 06:32:26 +0700

    hoan thanh chuc nang co ban

--- src/main/java/com/studentApp/Entity/Permission.java
+++ src/main/java/com/studentApp/Entity/Permission.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +36,7 @@ public class Permission {
 	@Column(name = "updated_at", nullable = false)
 	private LocalDateTime updatedAt;
 
-	@ManyToMany(mappedBy = "permissions")
+	@ManyToMany(mappedBy = "permissions", fetch = FetchType.LAZY)
 	@JsonBackReference
 	private List<Role> roles;
 }
