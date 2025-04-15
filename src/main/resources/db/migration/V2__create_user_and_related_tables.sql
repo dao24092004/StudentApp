c01a43f653f72b57572c650299040df9877cad29
dao24092004 <134945882+dao24092004@users.noreply.github.com>
2025-04-15 09:22:34 +0700

    Add CI pipeline with SpotBugs and Dependency-Check

--- src/main/resources/db/migration/V2__create_user_and_related_tables.sql
+++ src/main/resources/db/migration/V2__create_user_and_related_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE tbl_user (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    role_id  INTEGER ,
+    role_id  INTEGER DEFAULT 3,
     avatar_url VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
